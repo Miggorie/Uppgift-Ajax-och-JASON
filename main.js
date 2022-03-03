ae43128cee71d8d10cf092effc0a40a40e6854db
Miggorie <info@elinselin.com>
2022-03-03 13:28:05 +0100

    Try2

--- main.js
+++ main.js
@@ -1,23 +1,53 @@
-let input = $("#input");
+/**
+ *  OMDb template
+ *	Documentation: http://www.omdbapi.com/
+ *  Generate an API key here: http://www.omdbapi.com/apikey.aspx
+ */
+
+/**
+ * According to documentation, you need at least 2 parameters when calling the API http://www.omdbapi.com/
+ * 1 Required parameter: apikey
+ * 2 Required parameter: One of the following i=, t= or s=
+ *
+ *
+ * Example with parameter s=star trek
+ * http://www.omdbapi.com/?apikey=51741695&s=star trek
+ *
+ * Example with parameter s=star trek AND y=2020
+ * http://www.omdbapi.com/?apikey=[yourkey]&s=star trek&y=2020
+ *
+ * Example with parameter s=star trek AND type=series
+ * http://www.omdbapi.com/?apikey=[yourkey]&s=star trek&type=series
+ *
+ */
+
+// Send all data to: http://www.omdbapi.com/?apikey=51741695
+
+// Användaren skriver in en titel
+// Programmet söker efter filmen
+
+let userInput = $("#input");
 let fetchBtn = $("#fetchBtn");
-let tableBody = $("#table tbody");
+let ul = $("#ul");
 
 fetchBtn.on("click", function () {
-  let url =
-    "http://codexplained.se/lorem_text_random.php?numberOfWords=" + input.value;
+  let url = "http://www.omdbapi.com/?apikey=51741695&s=";
   console.log(url);
 
-  fetch(url)
-    .then((response) => response.text())
+  fetch(url + userInput)
+    .then((response) => response.json())
+
     .then((data) => {
-      tableBody.html(`      <tr> 						
-      <td> ${input.value}</td>				
-      <td> ${data} </td>
-  </tr>
-`);
-    })
-    .catch((error) => {
-      console.log(error);
+      const list = data;
+
+      list.map((item) => {
+        const name = item.l;
+      });
     });
-  input.value = "";
+  userInput.value = "";
 });
+
+// .catch((error => {console.error(error);});
+
+// const movie = <li><img src="${poster}"><h2>${name}</h2></li>
+//  document.querySelector('.movies').innerHTML+= movie;
